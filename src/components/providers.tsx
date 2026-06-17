414c13b24d3029fcacb160b8e2f10d05b4379f15
Fikri Mutaqin <fikrimutaqin@Fikri-MacBook-Pro.local>
2026-06-17 17:54:49 +0700

    chore(main): init project cms school

---
+++ src/components/providers.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { makeStore } from '@/lib/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  // Create a Redux store instance once per session/request
+  const [store] = useState(() => makeStore());
+
+  // Create a QueryClient instance once per session/request
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // Set default staleTime above 0 to avoid immediate refetching on client hydration
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  );
+}
+
